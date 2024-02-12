15d66732f223246e58be8c461c628baeeca8dc75
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-12 12:21:33 +0300

    working with json

--- pythonProject/json/bookJson.py
+++ pythonProject/json/bookJson.py
@@ -2,13 +2,18 @@ book = {}
 
 book['bob'] = {
     'author': 'Bob',
-    'title':'life journey',
-    'year':'2024',
-    'isbn':'0-001'
-},
+    'title': 'life journey',
+    'year': '2024',
+    'isbn': '0-001'
+}
 book['Stephen'] = {
     'author': 'Stephen',
-    'title':'mount kenya journey',
-    'year':'2020',
-    'isbn':'0-001'
+    'title': 'mount kenya journey',
+    'year': '2020',
+    'isbn': '0-002'
 }
+
+c = book['bob']['isbn']
+print(c)
+h = book['Stephen']['title']
+print(h)
--- pythonProject/json/importJson.py
+++ pythonProject/json/importJson.py
@@ -1,12 +1,12 @@
 import bookJson
 import json
 
-# s = json.dumps(bookJson.book)
-# print(s)
-#
-# # to write into a remote file
-# with open("./book.txt", "w") as b:
-#     b.write(s)
+s = json.dumps(bookJson.book)
+print(s)
+
+# to write into a remote file
+with open("./book.txt", "w") as b:
+    b.write(s)
 
 #to read from thr file
 read = open("./book.txt", "r")
@@ -18,4 +18,9 @@ print(f)
 k = json.loads(f)
 print(k)
 b = k["bob"]
+w = type(b)
+print(w)
 print(b)
+kk = bookJson.book['bob']
+print(type(kk))
+print(kk)
