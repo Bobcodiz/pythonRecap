895a1720c81ffe5e76ffdbb9743854edcf398f70
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-14 07:12:44 +0300

    sharing by value and array

--- pythonProject/multiprocessing/sharing.py
+++ pythonProject/multiprocessing/sharing.py
@@ -2,6 +2,7 @@ import multiprocessing
 
 
 def calc_square(numbers, result):
+    v.value = 987.0
     for idx, n in enumerate(numbers):
         result[idx] = n * n
     return
@@ -9,9 +10,13 @@ def calc_square(numbers, result):
 
 if __name__ == '__main__':
     numbers = [1, 2, 3, 4, 5]
+    # by array
     result = multiprocessing.Array("i", 5)
+    # by Value
+    v = multiprocessing.Value("d", 0.0)
     p = multiprocessing.Process(target=calc_square, args=(numbers, result))
     p.start()
     p.join()
 
     print("the result is :", result[:])
+    print(v.value)
