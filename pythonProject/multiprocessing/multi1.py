ef5f80e8290db633c74bb7d307abc9700a003f19
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-14 06:34:44 +0300

    ...

--- pythonProject/multiprocessing/multi1.py
+++ pythonProject/multiprocessing/multi1.py
@@ -2,26 +2,25 @@ import math
 import multiprocessing as mul
 import time
 
-arr = []
-
 
 def calc_square(numbers):
+
     for i in numbers:
         time.sleep(3)
-        print("the square of {} is {}".format(i, i * i))
+        print("square :", str(i * i))
 
 
 def calc_cube(numbers):
     for i in numbers:
         time.sleep(3)
-        print("the cube of {} is {}".format(i, math.pow(i, 3)))
+        print("cube : ", str(i * i * i))
 
 
 if __name__ == '__main__':
     arr = [1, 2, 3, 4, 5]
 
-    p1 = mul.Process(target=calc_square, args=arr, )
-    p2 = mul.Process(target=calc_cube, args=arr, )
+    p1 = mul.Process(target=calc_square, args=(arr, ))
+    p2 = mul.Process(target=calc_cube, args=(arr, ))
 
     p1.start()
     p2.start()
